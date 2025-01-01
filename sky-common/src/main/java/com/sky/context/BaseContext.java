a6fbea26fd06a1a9c85a805ac8c46be100e608c3
Emotional_bug <1541373309@qq.com>
2025-01-01 23:28:41 +0800

    新增员工

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -1,17 +1,21 @@
 package com.sky.context;
 
+//线程上下文
 public class BaseContext {
 
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
 
+    //存储当前用户的ID
     public static void setCurrentId(Long id) {
         threadLocal.set(id);
     }
 
+    //获取当前用户的ID
     public static Long getCurrentId() {
         return threadLocal.get();
     }
 
+    //移除当前用户
     public static void removeCurrentId() {
         threadLocal.remove();
     }
